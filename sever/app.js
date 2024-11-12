9965e6807806e86ed621667bedc335310c1a795c
dianakosta1985 <dianakosta@gmail.com>
2024-11-12 09:56:15 -0500

    set api

--- sever/app.js
+++ sever/app.js
@@ -14,13 +14,26 @@ app.use(express.json());
 // Enable CORS for specific origin
 app.use(
   cors({
-    origin: "http://localhost:3000", // Allow requests from Next.js app
+    origin: "http://localhost:5000", // Allow requests from Next.js app
+    method: ["POST", "GET"],
+    credentials: true,
   })
 );
 
+app.get("/", async (req, res) => {
+  res.send("api is running");
+});
+
 app.get("/pages", async (req, res) => {
-  const pageData = await PageData.findAll();
-  res.send(pageData);
+  try {
+    const pageData = await PageData.findAll();
+    res.send(pageData);
+  } catch (error) {
+    console.error("Error fetching page data:", error);
+    res
+      .status(500)
+      .send({ error: "An error occurred while fetching the page data." });
+  }
 });
 
 app.get("/pages/:pageId", async (req, res) => {
@@ -43,10 +56,17 @@ app.get("/pages/:pageId", async (req, res) => {
 });
 
 app.post("/pages", async (req, res) => {
-  await PageData.create(req.body);
-  res.send("pages are created");
+  try {
+    await PageData.create(req.body);
+    res.send("pages are created");
+  } catch (error) {
+    console.error("Error fetching page data:", error);
+    res
+      .status(500)
+      .send({ error: "An error occurred while fetching the page data." });
+  }
 });
 
-app.listen(3001, () => {
+app.listen(5000, () => {
   console.log(`Server is listning at port 3001`);
 });
